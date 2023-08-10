19fd827e968e443fabcae4ad62d6813c3bf25adc
LordAkirast <darkwarish@gmail.com>
2023-08-10 02:02:45 -0300

    Signup funciona corretamente. falta ajustes no cancalemanto so

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,12 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import { format } from 'date-fns'; // Para formatar a data
 import { useEffect } from 'react';
+import axios from 'axios';
 
 
 
 function App() {
-  const [inputValue, setInputValue] = useState('>'); // Estado inicial com o caractere '5'
+  const [inputValue, setInputValue] = useState('> '); // Estado inicial com o caractere '5'
   const [commandValue, setCommandValue] = useState(''); // Inicialize com null
   const [siteName, setsiteName] = useState('')
   const [location, setlocation] = useState('')
@@ -16,48 +17,205 @@ function App() {
   const [currentTime, setcurrentTime] = useState('')
   const [currentDate, setCurrentDate] = useState('');
   const [messages, setMessages] = useState([]);
+  const [lastCommand, setlastCommand] = useState('')
+
+  ///signup
+  const [name, setname] = useState('')
+  const [email, setemail] = useState('')
+  const [password, setpassword] = useState('')
+  const [confirmPassword, setconfirmPassword] = useState('')
 
   const handleInputChange = (event) => {
     setInputValue(event.target.value);
   };
 
-  const handleCommandInputKeyDown = (e) => {
+  const handleCommandInputKeyDown = async (e) => {
     if (e.key === 'Enter') {
       e.preventDefault();
-      if (!inputValue.includes('>')) {
-        setInputValue('>')
-        setMessages([...messages, 'ERROR: > NEEDED TO USE TERMINAL'])
-      } else {
-        console.log(inputValue)
 
-        switch (inputValue) {
-          case '> /log':
-            setInputValue('>')
-            setMessages([...messages, 'LOG: ' + messages])
-            break;
-          case '> /help':
-            setInputValue('>')
-            setMessages([...messages, '/log - Para ver o log de comandos. \n /help - Para ver todos os comandos. \n /login - Para realizar login. \n /signin - Para realizar cadastro. \n /logout - Para fazer logout. \n /home - Para navegar para a home. \n /clear - Limpa toda a tela.'])
-            break;
 
-          case '> /clear':
-            setInputValue('>')
-            setMessages([])
-            break;
+      ///se o comando for de signup entra aqui
+      if (lastCommand.includes('n4m3')) {
+        setname(inputValue.replace(/[\s>]+/g, ''))
+        setInputValue('')
+        setMessages(['Email: '])
+        setlastCommand('name')
+      }
+      else if (lastCommand.includes('name')) {
+        setemail(inputValue.replace(/[\s>]+/g, ''))
+        setInputValue('')
+        setMessages(['Password: '])
+        setlastCommand('email')
 
-          case '> /signin':
-            setInputValue('>')
-            setMessages([])
-            break;
+      } else if (lastCommand.includes('email') && name) {
+        setpassword(inputValue.replace(/[\s>]+/g, ''))
+        setInputValue('')
+        setMessages(['Confirm Password: '])
+        setlastCommand('password')
+      } else if (lastCommand.includes('password') && name && email) {
+        setconfirmPassword(inputValue.replace(/[\s>]+/g, ''))
+        setInputValue('')
+        setMessages(['PRESS ENTER TO CONTINUE: '])
+        setlastCommand('confirmPass')
+      } else if (lastCommand.includes('confirmPass') && name && email && password) {
+        setInputValue('')
+        setMessages(['Name: ', name, 'Email: ', email, 'Password: ', password, 'ConfirmPassword: ', confirmPassword, 'Y TO CONFIRM N TO DECLINE'])
+        setlastCommand('signComplete')
+      } else if (inputValue.includes('y') && lastCommand.includes('signComplete')) {
+        setMessages(['SENDING DATA... PRESS ENTER TO CONTINUE'])
 
-          default:
-            if (inputValue.trim() !== '') {
-              setMessages([...messages, `command: ${inputValue} not found.`]);
-              setInputValue('>');
-            }
+        const data = {
+          name,
+          email,
+          password,
+          confirmPassword,
+        };
+
+        try {
+          const response = await axios.post(`${import.meta.env.VITE_API_URL}/signup`, data);
+          console.log(response.data); // Trate a resposta da API conforme necessário
+          setname("")
+          setemail("")
+          setpassword("")
+          setconfirmPassword("")
+          setMessages(['SUCCESS: User created!'])
+          setInputValue('> ')
+
+
+
+        } catch (error) {
+          if (!name) {
+            setMessages(["ERROR: Name cannot be empty"])
+            setname("")
+            setemail("")
+            setpassword("")
+            setconfirmPassword("")
+            setlastCommand('error')
+            setInputValue('')
+          }
+
+          else if (!email) {
+            setMessages(["ERROR: Email cannot be empty"])
+            setname("")
+            setemail("")
+            setpassword("")
+            setconfirmPassword("")
+            setlastCommand('error')
+            setInputValue('')
+          }
+
+          else if (!password) {
+            setMessages(["ERROR: Password cannot be empty"])
+            setname("")
+            setemail("")
+            setpassword("")
+            setconfirmPassword("")
+            setlastCommand('error')
+            setInputValue('')
+          }
+
+          else if (!confirmPassword) {
+            setMessages(["ERROR: Confirm Passowrd cannot be empty"])
+            setname("")
+            setemail("")
+            setpassword("")
+            setconfirmPassword("")
+            setlastCommand('error')
+            setInputValue('')
+          }
+
+          else if (password.length < 3) {
+            setMessages(["ERROR: Password must be larger than 3 characters."])
+            setname("")
+            setemail("")
+            setpassword("")
+            setconfirmPassword("")
+            setlastCommand('error')
+            setInputValue('')
+          }
+
+          else if (password !== confirmPassword) {
+            setMessages(["ERROR: Password and Confirm Password must match."])
+            setname("")
+            setemail("")
+            setpassword("")
+            setconfirmPassword("")
+            setlastCommand('error')
+            setInputValue('> ')
+          }
+
+
+          console.error(error);
+          setMessages(['ERROR: ',error.response.data])
+          setname("")
+          setemail("")
+          setpassword("")
+          setconfirmPassword("")
+          setlastCommand('error')
+          setInputValue('> ')
         }
 
+        setlastCommand('')
 
+      } else if (inputValue.includes('n') && lastCommand.includes('signComplete')) {
+        alert('cancelar!')
+        setlastCommand('')
+      } else {
+
+
+        if (!inputValue.includes('>')) {
+          setInputValue('> ')
+          setMessages([...messages, 'ERROR: > NEEDED TO USE TERMINAL'])
+        } else {
+          console.log(inputValue)
+
+          switch (inputValue) {
+            case '> \log':
+              setInputValue('> ')
+              setMessages([...messages, 'LOG: ' + messages])
+              setlastCommand(inputValue)
+              break;
+            case '> \help':
+              setInputValue('> ')
+              setMessages([...messages, `log - See all history logs. |
+            help - See all commands. |
+            login - To login. |
+            signup - To register. |
+            logout - To logout. |
+            home - To go to main page. |
+            clear - Clear screen. |
+           `])
+              setlastCommand(inputValue)
+              break;
+
+            case '> \clear':
+              setInputValue('> ')
+              setMessages([])
+              setlastCommand(inputValue)
+              break;
+
+            case '> \signup':
+              setMessages(['NAME: '])
+              setlastCommand('n4m3')
+              setInputValue('')
+              break;
+
+
+            case '> \last':
+              setInputValue('> ')
+              setMessages([...messages, lastCommand])
+              break;
+
+            default:
+
+              if (inputValue.trim() !== '') {
+                setMessages([...messages, `command: ${inputValue} not found. Use 'help' to see commands.`]);
+                setInputValue('> ');
+              }
+          }
+
+
+        }
       }
     }
   };
@@ -162,7 +320,7 @@ function App() {
           <p>Date: {currentDate}</p>
           <p>Time: {currentTime}</p>
           <p>Location: {location}</p>
-          <p>User: Guest</p>
+          <p>User: {name}</p>
           <p>==============================</p>
         </header>
         <div className='message-container' style={{ marginTop: '60px' }}>
